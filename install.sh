c1c0b6a684dd215fb7a887635e291d4b6b6327e7
Diego Kiedanski <dkiedanski@tryolabs.com>
2022-01-08 14:47:49 -0300

    working copy with uninstall

---
+++ install.sh
@@ -0,0 +1,25 @@
+echo "Installing the project"
+
+set -a
+source variables
+envsubst < variables > .variables
+
+set -a
+source .variables
+
+UDEV_FILE="/etc/udev/rules.d/10-${PROJECT_NAME}.rules"
+envsubst < template.rules | sudo tee "$UDEV_FILE" > /dev/null
+
+SCRIPT_FILE="/usr/local/bin/${PROJECT_NAME}.sh"
+envsubst '$LOCAL_DIR $ABSOLUTE_REMOTE_DIR $MOUNT_FILE $NODE_FILE $PIPE_FILE $LOG_FILE $USER' < sync_template.sh | sudo tee "$SCRIPT_FILE" > /dev/null
+sudo chmod +x "$SCRIPT_FILE"
+
+
+BASH_PATH=$(which bash)
+[[ "`tail -n1 /etc/rc.local`" == "exit 0" ]] && sudo sed -i -e "\$i "${BASH_PATH}" "${SCRIPT_FILE}" \&" /etc/rc.local
+
+echo "Creating uninstall file"
+echo "sudo rm $UDEV_FILE" > uninstall.sh
+echo "sudo rm $SCRIPT_FILE" >> uninstall.sh
+echo "sudo sed -i -e \"/$PROJECT_NAME/d\" /etc/rc.local" >> uninstall.sh
+chmod +x uninstall.sh
---
+++ sync_template.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+[[ ! -d "$LOCAL_DIR" ]] && sudo mkdir "$LOCAL_DIR" && sudo chown -R "$USER:$USER" "$LOCAL_DIR" 2>"$LOG_FILE"
+[[ ! -d "$MOUNT_FILE" ]] && sudo mkdir "$MOUNT_FILE" && sudo chown -R "$USER:$USER" "$MOUNT_FILE" 2>"$LOG_FILE"
+
+
+doSynchronization()
+{
+  sudo mount "$NODE_FILE" "$MOUNT_FILE" >> "$LOG_FILE" 2>&1
+  rsync -rtDvz --ignore-existing $LOCAL_DIR $ABSOLUTE_REMOTE_DIR >> "$LOG_FILE" 2>&1
+  sudo umount "$MOUNT_FILE"
+}
+
+echo "Starting Kindle Updater" >> "$LOG_FILE"
+
+trap "rm -f $PIPE_FILE" EXIT
+
+#If the pipe doesn't exist, create it
+if [[ ! -p $PIPE_FILE ]]; then
+    mkfifo $PIPE_FILE
+fi
+
+#If the disk is already plugged on startup, do a syn
+if [[ -e "$NODE_FILE" ]]
+then
+    doSynchronization
+fi
+
+#Make the permanent loop to watch the usb connection
+while true
+do
+    if read line <$PIPE_FILE; then
+        #Test the message read from the fifo
+        if [[ "$line" == "connected" ]]
+        then
+            #The usb has been plugged, wait for disk to be mounted by KDE
+            while [[ ! -e "$NODE_FILE" ]]
+            do
+                sleep 1
+            done
+            doSynchronization
+        else
+            echo "Unhandled message from fifo : [$line]"
+        fi
+    fi
+done
+echo "Reader exiting"
